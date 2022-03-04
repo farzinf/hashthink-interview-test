9f773b9dad9c65ebb4d1cca94348a1004631d112
farzin <farzin.falahati@gmail.com>
2022-03-04 10:00:34 +0330

    Add gallery migration

---
+++ src/migrations/20220303070845-gallery-create.migration.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('galleries', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      companyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'companies',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+
+      // Timestamps
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE,
+    })
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('galleries');
+  },
+}
